0ae3fba304e197c70fdf1ca7d56db0d2a5a1b677
Hecherat <hecherat@gmail.com>
2025-02-02 14:54:24 -0300

    feat: [develop] - Add MealsSlug page

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,22 +1,52 @@
-import Link from "next/link";
+import { Suspense } from "react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-type MealsSlugPage = {
-  params: {
-    mealSlug: string;
-  };
-};
+import { getMeal } from "@/lib/meals";
+
+import classes from "./page.module.css";
+
+type MealsSlugPage = Readonly<{
+  params: Promise<{ mealSlug: string }>;
+}>;
 
 export default async function MealDetailsPage({ params }: MealsSlugPage) {
-  const meal = await params.mealSlug;
+  const { mealSlug } = await params;
+
+  const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Meal - {meal} - Details
-      </h1>
-      <div style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-        <Link href="/meals">back to meals</Link>
-      </div>
-    </main>
+    <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image
+            src={meal.image}
+            alt={meal.title}
+            sizes="15rem"
+            fill
+            priority
+          />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br />") ?? "",
+          }}
+        />
+      </main>
+    </Suspense>
   );
 }
